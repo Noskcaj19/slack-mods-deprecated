9d6b3b201f1f0c5e7a445d1c9d783e2fd45fa66d
Noskcaj <kcajdev@gmail.com>
2017-11-08 19:32:08 -0500

    Improved install script, added uninstall ability

--- install.py
+++ install.py
@@ -1,5 +1,8 @@
 import os
 import sys
+import re
+import shutil
+import argparse
 
 COMMON_LINUX_SLACK_LOCATIONS = [
     "/usr/lib/slack/resources/app.asar.unpacked/src/static",
@@ -22,26 +25,79 @@ def verify_install_path(path):
         return False
     return True
 
-def main():
+def strip_code(script):
+    return re.sub("\n?\n?////SLACK MODS START////.*////SLACK MODS END////", "", script, flags=re.S)
+
+def uninstall(verbose=False):
     install_path = get_slack_install_path()
     if not verify_install_path(install_path):
         print("Unable to locate valid Slack install path")
         exit(1)
     ssb = install_path + '/ssb-interop.js'
+    backup_path = install_path + '/ssb-interop-backup.js'
+
+    if not os.path.exists(backup_path):
+        print("Unable to locate backup preload script, attempting to scrub code")
+        with open(ssb, 'r') as f:
+            clean = strip_code(f.read())
+        with open(ssb, 'w') as f:
+            f.write(clean)
+        shutil.copymode(install_path + "/ssb-interop-lite.js", ssb)
+    else:
+        if verbose:
+            print("Replacing preload script with backup")
+        shutil.copyfile(backup_path, ssb)
+        shutil.copymode(backup_path, ssb)
+    print("Uninstalled slack-mods")
+
+def install(verbose=False):
+    install_path = get_slack_install_path()
+    if not verify_install_path(install_path):
+        print("Unable to locate valid Slack install path")
+        exit(1)
+    ssb = install_path + '/ssb-interop.js'
+    print("Installing to {}".format(ssb))
+
+    # Check if slack-mods is already installed
     with open(ssb) as f:
         content = f.read()
         if "////SLACK MODS START////" in content:
             print("Detected existing install, exiting.")
             exit(1)
-    with open(ssb, 'a') as f:
-        f.write(launch_script)
 
+    # Make a backup
+    backup_path = install_path + '/ssb-interop-backup.js'
+    if verbose:
+        print("Making backup preload script at {}", backup_path)
+    shutil.copyfile(ssb, backup_path)
+    shutil.copymode(ssb, backup_path)
+
+    # Do the install
+    with open(ssb, 'a') as f:
+        f.write(preload_script)
+
+    # Create mod folder if it does not exist
     mods_path = os.path.expanduser("~/.slack_mods")
     if not os.path.exists(mods_path):
+        print("`~/.slack_mods` does not exist, creating it now")
         os.makedirs(mods_path)
+    elif verbose:
+        print("`~/.slack_mods` already exists")
+
     print("Slack mods has been installed. Please quit and reopen Slack.")
 
-launch_script = """
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-u', action='store_true', help='Uninstall slack-mods')
+    parser.add_argument('-v', action='store_true', help='Enable verbose printing')
+    args = parser.parse_args()
+    if not args.u:
+        install(args.v)
+    else:
+        uninstall(args.v)
+
+
+preload_script = """
 
 ////SLACK MODS START////
 // ** slack-plugins ** https://github.com/Noskcaj19/slack-mods
